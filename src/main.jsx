5f712eb52e5a7db1ac520c39403f0aa3df7f6de6
alrakib581 <alrakib581@gmail.com>
2023-04-11 22:44:39 +0600

    all route setup complete in main jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Main from '../src/components/Main/Main'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Main from './components/Main/Main';
-
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Error from './components/Error/Error';
+import Blogs from './components/Blogs/Blogs';
+import JobDetails from './components/JobDetails/JobDetails';
+import AppLied from './components/AppLied/AppLied';
+// creating route herer 
 const router = createBrowserRouter([
   {
     path: '/',
@@ -14,18 +20,38 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Home></Home>
-      }
+        element: <Home></Home>,
+        loader: ()=> fetch('/data.json')
+      },
+      {
+        path:'/statistics',
+        element: <Statistics></Statistics>
+      },
+      { 
+        path: '/jobdet/:id',
+        element: <JobDetails></JobDetails>,
+        loader: ()=> fetch('/data.json')
+      },
+      {
+        path:'/appliedJobs',
+        element: <AppLied></AppLied>,
+        loader: ()=> fetch('/data.json')
+      },
+      {
+        path:'/blog',
+        element: <Blogs></Blogs>
+      },
     ]
   },
   {
     path: '*',
     element: <Error></Error>
   }
-]);
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
