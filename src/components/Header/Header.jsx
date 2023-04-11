69740a2e512688e2c275708d6db6c316778da5fc
alrakib581 <alrakib581@gmail.com>
2023-04-11 22:09:45 +0600

    header code and some json file added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h1>ffff</h1>
+        <div className='header position-sticky top-0'>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary py-4 ">
+                <div className="container">
+                     <Link className="navbar-brand fw-bold" to="/">Job Hunt</Link>
+                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                     <span className="navbar-toggler-icon"></span>
+                     </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav  gap-md-4 m-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/">Home</Link> 
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/statistics">Statistics</Link> 
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" to="/appliedJobs">Applied Jobs</Link>
+                     </li>
+                    <li className="nav-item">
+                         <Link className="nav-link active" to='/blog' >Blog</Link>
+                    </li>
+
+                </ul>
+                     <button className="custom-btn" type="submit">Start Applying</button>
+                </div>
+            </div>
+            </nav>     
         </div>
     );
 };
